4610680763da0b959c6985ae1635dc71f0adf129
VioletLing <50896903+VioletLing@users.noreply.github.com>
2019-12-08 13:59:35 +0800

    Update sniffer.py

--- pcap/sniffer.py
+++ pcap/sniffer.py
@@ -7,12 +7,11 @@ from scapy.utils import PcapReader
 while True:
     dpkt = sniff(count=1)
     dpkt.setfilter = 'TCP'
-    wrpcap("test.pcap", dpkt)
-    packets = scapy.rdpcap('test.pcap')
+    wrpcap("data.pcap", dpkt)
+    packets = scapy.rdpcap('data.pcap')
     for head in packets:
         if 'TCP' in head:
             s = repr(head)
             print(s)
             # print("数据报的源地址为："+head['IP'].src)
             # print("数据报的目的地址为："+head['IP'].dst)
-            #
